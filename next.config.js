4b1191dd2059c5cd5027716136da6aa0a9e9eb68
Kaspar Kallas <kaspar@superfluid.finance>
2022-05-16 14:10:50 +0300

    Fix Link.tsx and make project buildable without Sentry token

--- next.config.js
+++ next.config.js
@@ -3,29 +3,37 @@
 // https://nextjs.org/docs/api-reference/next.config.js/introduction
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-const { withSentryConfig } = require('@sentry/nextjs');
+const { withSentryConfig } = require("@sentry/nextjs");
 
 /** @type {import('next').NextConfig} */
 const moduleExports = {
   reactStrictMode: true,
   images: {
     loader: "custom",
-    domains: ['raw.githubusercontent.com']
+    domains: ["raw.githubusercontent.com"],
   },
 };
 
-const sentryWebpackPluginOptions = {
-  // Additional config options for the Sentry Webpack plugin. Keep in mind that
-  // the following options are set automatically, and overriding them is not
-  // recommended:
-  //   release, url, org, project, authToken, configFile, stripPrefix,
-  //   urlPrefix, include, ignore
+const SENTRY_AUTH_TOKEN = process.env.SENTRY_AUTH_TOKEN;
 
-  silent: true, // Suppresses all logs
-  // For all available options, see:
-  // https://github.com/getsentry/sentry-webpack-plugin#options.
-};
+if (SENTRY_AUTH_TOKEN) {
+  const sentryWebpackPluginOptions = {
+    // Additional config options for the Sentry Webpack plugin. Keep in mind that
+    // the following options are set automatically, and overriding them is not
+    // recommended:
+    //   release, url, org, project, authToken, configFile, stripPrefix,
+    //   urlPrefix, include, ignore
 
-// Make sure adding Sentry options is the last code to run before exporting, to
-// ensure that your source maps include changes from all other Webpack plugins
-module.exports = withSentryConfig(moduleExports, sentryWebpackPluginOptions);
+    silent: true, // Suppresses all logs
+    // For all available options, see:
+    // https://github.com/getsentry/sentry-webpack-plugin#options.
+  };
+  // Make sure adding Sentry options is the last code to run before exporting, to
+  // ensure that your source maps include changes from all other Webpack plugins
+  module.exports = withSentryConfig(moduleExports, sentryWebpackPluginOptions);
+} else {
+  console.warn(
+    "Sentry release not created because SENTRY_AUTH_TOKEN is not set."
+  );
+  module.exports = moduleExports;
+}
