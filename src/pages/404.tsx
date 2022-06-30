6289ca33c27c57da1f46ff69773924f35e3f6a63
Mikk <mikk@superfluid.finance>
2022-06-30 14:37:45 +0300

    404 page improvements.

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,35 +1,55 @@
 import { Button, Container, Typography } from "@mui/material";
 import { NextPage } from "next";
 import ArrowBackRoundedIcon from "@mui/icons-material/ArrowBackRounded";
+import Link from "next/link";
+
+const HELP_CENTER_URL = "https://help.superfluid.finance/";
 
 const Page404: NextPage = () => {
   return (
-    <Container maxWidth="lg">
+    <Container maxWidth="lg" sx={{ textAlign: "center", mt: "20vh" }}>
       <Typography variant="h1" color="primary">
         Oops!
       </Typography>
-      <Typography variant="h1">Something went wrong.</Typography>
+      <Typography variant="h1" sx={{ mb: 2 }}>
+        Something went wrong.
+      </Typography>
       <Typography variant="h4">
-        Error
+        Error{" "}
         <Typography variant="h4" color="primary" component="span">
           404
         </Typography>{" "}
         Page Not Found
       </Typography>
 
-      <Button
-        variant="contained"
-        color="primary"
-        startIcon={<ArrowBackRoundedIcon />}
-      >
-        Return to Dashboard
-      </Button>
+      <Link href="/" passHref>
+        <Button
+          variant="contained"
+          color="primary"
+          size="large"
+          component="a"
+          startIcon={<ArrowBackRoundedIcon />}
+          sx={{ my: 4.5 }}
+          href="/"
+        >
+          Return to Dashboard
+        </Button>
+      </Link>
 
       <Typography variant="h5">
         Need support? Visit our{" "}
-        <Typography color="primary" variant="h5" component="span">
-          Help Center
-        </Typography>
+        <Link href={HELP_CENTER_URL} passHref target="_blank">
+          <Typography
+            color="primary"
+            variant="h5"
+            component="a"
+            target="_blank"
+            href={HELP_CENTER_URL}
+            sx={{ textDecoration: "none" }}
+          >
+            Help Center
+          </Typography>
+        </Link>
       </Typography>
     </Container>
   );
