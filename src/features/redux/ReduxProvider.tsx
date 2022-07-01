1046fa581a36b17a11614cd53ed5c371f783527c
Kaspar Kallas <kaspar@superfluid.finance>
2022-07-01 10:56:35 +0300

    Use signer to create SDK-core's framework

--- src/features/redux/ReduxProvider.tsx
+++ src/features/redux/ReduxProvider.tsx
@@ -1,15 +1,18 @@
+import { Framework } from "@superfluid-finance/sdk-core";
 import {
   initiateOldPendingTransactionsTrackingThunk,
   setFrameworkForSdkRedux,
 } from "@superfluid-finance/sdk-redux";
+import { BigNumber } from "ethers";
 import { FC, useEffect } from "react";
 import { Provider } from "react-redux";
-import { useAccount, useNetwork, useSigner } from "wagmi";
+import { useAccount, useNetwork, useProvider, useSigner } from "wagmi";
 import { networks } from "../network/networks";
 import readOnlyFrameworks from "../network/readOnlyFrameworks";
 import { reduxStore, useAppDispatch } from "./store";
 
 const ReduxProviderCore: FC = ({ children }) => {
+  const { data: signer } = useSigner();
   const { address: accountAddress } = useAccount();
 
   const dispatch = useAppDispatch();
@@ -25,12 +28,23 @@ const ReduxProviderCore: FC = ({ children }) => {
     }
   }, [accountAddress]);
 
-  // TODO(KK): Use wagmi's providers. Wagmi might be better at creating providers and then we don't create double providers.
   useEffect(() => {
+    // TODO(KK): Use wagmi's providers. Wagmi might be better at creating providers and then we don't create double providers.
     readOnlyFrameworks.map((x) =>
       setFrameworkForSdkRedux(x.chainId, x.frameworkGetter)
     );
-  }, []);
+
+    if (signer) {
+      signer.getChainId().then((chainId) => {
+        setFrameworkForSdkRedux(chainId, () =>
+          Framework.create({
+            chainId: chainId,
+            provider: signer,
+          })
+        );
+      });
+    }
+  }, [signer]);
 
   return <>{children}</>;
 };
