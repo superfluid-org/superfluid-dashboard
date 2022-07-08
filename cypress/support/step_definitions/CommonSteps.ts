a14d6c07ae8530021127fb5f41a11bbeb2093e65
elvijsTDL <elvijs.dzirkals@testdevlab.com>
2022-07-08 14:34:18 +0300

    Use arrow functions in step definitions

--- cypress/support/step_definitions/CommonSteps.ts
+++ cypress/support/step_definitions/CommonSteps.ts
@@ -4,61 +4,61 @@ import {DashboardPage} from "../../pageObjects/pages/DashboardPage";
 import {WrapPage} from "../../pageObjects/pages/WrapPage";
 import {SendPage} from "../../pageObjects/pages/SendPage";
 
-Given(/^"([^"]*)" is open without connecting a wallet$/, function (page: string) {
+Given(/^"([^"]*)" is open without connecting a wallet$/,  (page: string) => {
     Common.openPage(page);
 });
 
-Given(/^User clicks on the "([^"]*)" navigation button$/, function (button: string) {
+Given(/^User clicks on the "([^"]*)" navigation button$/,  (button: string) => {
     Common.clickNavBarButton(button);
 });
 
-Then(/^Dashboard page is open when wallet of the user is not connected$/, function () {
+Then(/^Dashboard page is open when wallet of the user is not connected$/,  () => {
     DashboardPage.checkIfDashboardConnectIsVisible();
 });
 
-Then(/^Wrap\/Unwrap page is open and the wrap container is visible$/, function () {
+Then(/^Wrap\/Unwrap page is open and the wrap container is visible$/,  () => {
     WrapPage.checkIfWrapContainerIsVisible();
 });
 
-Then(/^Send page is open and the send container is visible$/, function () {
+Then(/^Send page is open and the send container is visible$/,  () => {
     SendPage.checkIfSendContainerIsVisible();
 });
 
-Then(/^Wait for (\d+) seconds$/, function (seconds: number) {
+Then(/^Wait for (\d+) seconds$/,  (seconds: number) => {
     cy.wait(seconds * 1000);
 });
 
-Given(/^User clicks on the connect wallet button$/, function () {
+Given(/^User clicks on the connect wallet button$/,  () => {
     Common.clickConnectWallet();
 });
 
 Given(/^"([^"]*)" is open with a mocked connection to "([^"]*)" on "([^"]*)"$/,
-    function (page: string, account: string, network: string) {
+     (page: string, account: string, network: string) => {
         Common.openPage(page, true, account, network);
     });
 
-Given(/^User connects their wallet to the dashboard$/, function () {
+Given(/^User connects their wallet to the dashboard$/,  () => {
     Common.clickConnectWallet();
     Common.clickInjectedWallet();
 });
 
-Given(/^User changes their network to "([^"]*)"$/, function (network: string) {
+Given(/^User changes their network to "([^"]*)"$/,  (network: string) => {
     Common.changeNetwork(network);
 });
 
 Given(/^The navigation drawer shows that "([^"]*)" is "([^"]*)"$/,
-    function (account: string, message: string) {
+     (account: string, message: string) => {
     Common.checkNavBarWalletStatus(account, message);
 });
 
-Given(/^The navigation drawer shows connect wallet button$/, function () {
+Given(/^The navigation drawer shows connect wallet button$/,  () => {
     Common.drawerConnectWalletButtonIsVisible();
 });
 
-Given(/^User uses view mode to look at "([^"]*)"$/, function (account: string) {
+Given(/^User uses view mode to look at "([^"]*)"$/,  (account: string) => {
     Common.viewAccount(account);
 });
 
-Then(/^The stop viewing as an address button is visible$/, function () {
+Then(/^The stop viewing as an address button is visible$/,  () => {
     WrapPage.isStopViewingButtonVisible();
 });
