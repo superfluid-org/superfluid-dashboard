1f2f5099398b850203fd9383c2581783d574c501
Kaspar Kallas <kaspar@superfluid.finance>
2022-07-26 15:56:03 +0300

    Small optimization

--- src/features/tokenWrapping/useTokenPairsQuery.tsx
+++ src/features/tokenWrapping/useTokenPairsQuery.tsx
@@ -2,6 +2,7 @@ import { getNetworkDefaultTokenPair, Network } from "../network/networks";
 import { subgraphApi } from "../redux/store";
 
 export const useTokenPairsQuery = ({ network }: { network: Network }) => {
+  const defaultTokenPair = getNetworkDefaultTokenPair(network);
   return subgraphApi.useTokenUpgradeDowngradePairsQuery(
     {
       chainId: network.id,
@@ -9,8 +10,8 @@ export const useTokenPairsQuery = ({ network }: { network: Network }) => {
     {
       selectFromResult: (result) => ({
         ...result,
-        data: result.data ?? [getNetworkDefaultTokenPair(network)], // Doing it this way the list is never empty and always contains the default token pairs.
-        currentData: result.currentData ?? [getNetworkDefaultTokenPair(network)],
+        data: result.data ?? [defaultTokenPair], // Doing it this way the list is never empty and always contains the default token pairs.
+        currentData: result.currentData ?? [defaultTokenPair],
       }),
     }
   );
