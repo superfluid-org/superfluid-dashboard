a2e44b1e06fd6bf9c74a6e61fce776fcaca7cce2
Mikk <mikk@superfluid.finance>
2022-07-27 14:45:45 +0300

    Added some comments.

--- src/features/redux/ReduxPersistGate.tsx
+++ src/features/redux/ReduxPersistGate.tsx
@@ -7,6 +7,7 @@ import { reduxPersistor } from "./store";
  * Waits for Redux state to be restored from persistance.
  * Read more: https://github.com/rt2zz/redux-persist
  * NOTE: It is critical for RTK-Query to have PersistGate around it when using persistance (had some weird race-condition anomalies otherwise where the cache didn't invalidate).
+ * NOTE: While building static assets (SSR), the persistance is removed via function children and can be handled deeper in the app.
  */
 const ReduxPersistGate: FC = ({ children }) => {
   return (
