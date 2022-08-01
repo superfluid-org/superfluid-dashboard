3e170b837bd9f56b819bf44c265bf44feca368f5
Mikk <mikk@superfluid.finance>
2022-08-01 11:35:06 +0300

    More network badges and transaction drawer improvements.

--- src/features/network/NetworkBadge.tsx
+++ src/features/network/NetworkBadge.tsx
@@ -1,18 +1,20 @@
 import { Box, SxProps, Theme, Tooltip, TooltipProps } from "@mui/material";
 import { FC } from "react";
-import NetworkIcon from "./NetworkIcon";
+import NetworkIcon, { NetworkIconProps } from "./NetworkIcon";
 import { Network } from "./networks";
 
 interface NetworkBadgeProps {
   network: Network;
   sx?: SxProps<Theme>;
   TooltipProps?: Partial<TooltipProps>;
+  NetworkIconProps?: Omit<NetworkIconProps, "network">;
 }
 
 const NetworkBadge: FC<NetworkBadgeProps> = ({
   network,
   sx = {},
   TooltipProps = {},
+  NetworkIconProps = {},
 }) => (
   <Tooltip arrow title={network.name} placement="top" {...TooltipProps}>
     <Box sx={sx}>
@@ -20,6 +22,7 @@ const NetworkBadge: FC<NetworkBadgeProps> = ({
         network={network}
         size={24}
         fontSize={14}
+        {...NetworkIconProps}
         sx={{
           p: 0.25,
           backgroundColor: network.color,
