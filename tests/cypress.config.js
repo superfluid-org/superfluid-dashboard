9e6046b2cf63fc3f194fdfea85c06444f5c89732
elvijsTDL <elvijs.dzirkals@testdevlab.com>
2022-08-16 15:02:08 +0300

    Add project id

--- tests/cypress.config.js
+++ tests/cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    projectId: "2aaadn",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
