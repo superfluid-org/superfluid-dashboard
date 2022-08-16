371980cd8226f075acc00606706628783abb9e63
elvijsTDL <elvijs.dzirkals@testdevlab.com>
2022-08-16 10:59:38 +0300

    Move some config out of workflow

--- cypress.config.ts
+++ cypress.config.ts
@@ -29,7 +29,7 @@ export default defineConfig({
             }
         },
         projectId: "2aaadn",
-        baseUrl: "http://localhost:3000",
+        baseUrl: "https://deploy-preview-179--superfluid-dashboard.netlify.app",
         excludeSpecPattern: "*.js",
         viewportHeight: 1080,
         viewportWidth: 1920,
