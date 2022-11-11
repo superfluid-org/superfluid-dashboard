36fb05ba274b6a16f019d58af19fdec838912f5c
Kaspar Kallas <kaspar@superfluid.finance>
2022-11-11 12:05:07 +0200

    Another title style

--- src/features/vesting/VestingLayout.tsx
+++ src/features/vesting/VestingLayout.tsx
@@ -1,27 +1,26 @@
-import { Box, Container, Typography } from "@mui/material";
+import { Box, Container, Divider, Stack, Typography } from "@mui/material";
 import { FC, PropsWithChildren } from "react";
 import { useExpectedNetwork } from "../network/ExpectedNetworkContext";
 
 export const VestingLayout: FC<
-  PropsWithChildren<{
-    showSubtitle?: boolean;
-  }>
-> = ({ children, showSubtitle }) => {
+  PropsWithChildren
+> = ({ children }) => {
   const { network } = useExpectedNetwork();
 
   return (
     <Container key={`${network.slugName}`} maxWidth="lg">
-      <Box sx={{ mb: 5 }}>
-        <Typography component="h1" variant="h4" textAlign="center">
-          Vesting
-        </Typography>
-        {/* {showSubtitle && ( */}
-        <Typography variant="subtitle1" textAlign="center">
-          You can now vest using Superfluid streams!
-        </Typography>
-        {/* )} */}
-      </Box>
-      {children}
+      <Stack gap={4.5}>
+        <Box>
+          <Typography component="h1" variant="h3">
+            Vesting
+          </Typography>
+            <Typography variant="subtitle1">
+              You can now vest using Superfluid streams!
+            </Typography>
+        </Box>
+        <Divider />
+        {children}
+      </Stack>
     </Container>
   );
 };
