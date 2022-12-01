5c65eed532b11b2e79ceff1c9182dabb0cf84c00
Kaspar Kallas <kaspar@superfluid.finance>
2022-12-01 15:56:34 +0200

    Update StreamScheduler to FlowScheduler contract

--- src/eth-sdk/client/cjs/index.js
+++ src/eth-sdk/client/cjs/index.js
@@ -5,7 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.getGoerliSdk = exports.getContract = void 0;
 const ethers_1 = require("ethers");
-const streamScheduler_json_1 = __importDefault(require("../../abis/goerli/streamScheduler.json"));
+const flowScheduler_json_1 = __importDefault(require("../../abis/goerli/flowScheduler.json"));
 const vestingScheduler_json_1 = __importDefault(require("../../abis/goerli/vestingScheduler.json"));
 function getContract(address, abi, defaultSignerOrProvider) {
     return new ethers_1.Contract(address, abi, defaultSignerOrProvider);
@@ -13,7 +13,7 @@ function getContract(address, abi, defaultSignerOrProvider) {
 exports.getContract = getContract;
 function getGoerliSdk(defaultSignerOrProvider) {
     return {
-        "streamScheduler": getContract('0x7D37D9494a09E47e58B1F535386Ca4D9D175f23e', streamScheduler_json_1.default, defaultSignerOrProvider),
+        "flowScheduler": getContract('0x5b2D8d18FE90D840cbc012a8a06C3EeAA5cBe1a6', flowScheduler_json_1.default, defaultSignerOrProvider),
         "vestingScheduler": getContract('0x6f54e4744b13879482b5a487e832b23e566661b5', vestingScheduler_json_1.default, defaultSignerOrProvider),
     };
 }
--- src/eth-sdk/client/esm/index.js
+++ src/eth-sdk/client/esm/index.js
@@ -1,12 +1,12 @@
 import { Contract } from 'ethers';
-import goerli_streamScheduler_abi from '../../abis/goerli/streamScheduler.json';
+import goerli_flowScheduler_abi from '../../abis/goerli/flowScheduler.json';
 import goerli_vestingScheduler_abi from '../../abis/goerli/vestingScheduler.json';
 export function getContract(address, abi, defaultSignerOrProvider) {
     return new Contract(address, abi, defaultSignerOrProvider);
 }
 export function getGoerliSdk(defaultSignerOrProvider) {
     return {
-        "streamScheduler": getContract('0x7D37D9494a09E47e58B1F535386Ca4D9D175f23e', goerli_streamScheduler_abi, defaultSignerOrProvider),
+        "flowScheduler": getContract('0x5b2D8d18FE90D840cbc012a8a06C3EeAA5cBe1a6', goerli_flowScheduler_abi, defaultSignerOrProvider),
         "vestingScheduler": getContract('0x6f54e4744b13879482b5a487e832b23e566661b5', goerli_vestingScheduler_abi, defaultSignerOrProvider),
     };
 }
