26c22177b09b0801c6591c34fd608dec8ed9a00e
Kaspar Kallas <kaspar@superfluid.finance>
2023-01-13 13:01:47 +0200

    Use vesting scheduler contract address, not flow scheduler's

--- src/features/vesting/VestingSchedulerAllowances.tsx
+++ src/features/vesting/VestingSchedulerAllowances.tsx
@@ -105,7 +105,7 @@ export const VestingSchedulerAllowances: FC = () => {
   );
 
   const contractAddressLink = network.getLinkForAddress(
-    network.flowSchedulerContractAddress!
+    network.vestingSchedulerContractAddress!
   );
 
   return (
