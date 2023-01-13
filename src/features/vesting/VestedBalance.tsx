5a299d6d54e971bc7760c538a18b0e4b75eb7a50
Mikk <mikk@superfluid.finance>
2023-01-13 12:43:32 +0200

    Separate func for vesting balance

---
+++ src/features/vesting/VestedBalance.tsx
@@ -0,0 +1,63 @@
+import { getUnixTime } from "date-fns";
+import { BigNumber } from "ethers";
+import { FC, useMemo } from "react";
+import FlowingBalance from "../token/FlowingBalance";
+import { VestingSchedule } from "./types";
+
+interface VestedBalanceProps {
+  vestingSchedule: VestingSchedule;
+}
+
+const VestedBalance: FC<VestedBalanceProps> = ({ vestingSchedule }) => {
+  const {
+    startDate: startDateUnix,
+    cliffDate: cliffDateUnix,
+    endDate: endDateUnix,
+    cliffAmount,
+    flowRate,
+  } = vestingSchedule;
+
+  const unixNow = useMemo(() => getUnixTime(new Date()), []);
+
+  const currentFlowRate = useMemo(
+    () =>
+      unixNow > Number(cliffDateUnix || startDateUnix) &&
+      unixNow < Number(endDateUnix)
+        ? flowRate
+        : "0",
+    [cliffDateUnix, startDateUnix, endDateUnix, unixNow, flowRate]
+  );
+
+  const currentBalance = useMemo(() => {
+    const streamStartUnix = Number(cliffDateUnix || startDateUnix);
+    const streamedSeconds = Math.min(
+      unixNow - streamStartUnix,
+      Number(endDateUnix) - streamStartUnix
+    );
+
+    if (streamedSeconds < 0) return "0";
+
+    return BigNumber.from(flowRate)
+      .mul(streamedSeconds)
+      .add(cliffAmount || "0")
+      .toString();
+  }, [
+    cliffDateUnix,
+    startDateUnix,
+    endDateUnix,
+    unixNow,
+    cliffAmount,
+    flowRate,
+  ]);
+
+  return (
+    <FlowingBalance
+      balance={currentBalance}
+      flowRate={currentFlowRate}
+      balanceTimestamp={unixNow}
+      disableRoundingIndicator
+    />
+  );
+};
+
+export default VestedBalance;
