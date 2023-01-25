3b30366120b249bd7f27ebc51721bd55952dda2f
Mikk <mikk@superfluid.finance>
2023-01-25 11:32:46 +0200

    Auto stash before merge of "vesting-UI-improvements" and "origin/vesting-UI-improvements"

--- src/features/vesting/VestingScheduleTables.tsx
+++ src/features/vesting/VestingScheduleTables.tsx
@@ -14,17 +14,66 @@ import NoContentPaper from "../../components/NoContent/NoContentPaper";
 import { getAddress } from "../../utils/memoizedEthersUtils";
 import { vestingSubgraphApi } from "../../vesting-subgraph/vestingSubgraphApi";
 import { CopyIconBtn } from "../common/CopyIconBtn";
+import Link from "../common/Link";
 import { useExpectedNetwork } from "../network/ExpectedNetworkContext";
+import { Network } from "../network/networks";
 import {
   mapPendingToVestingSchedule,
   useAddressPendingVestingSchedules,
 } from "../pendingUpdates/PendingVestingSchedule";
+import { platformApi } from "../redux/platformApi/platformApi";
 import { useVisibleAddress } from "../wallet/VisibleAddressContext";
 import AggregatedVestingSchedules from "./AggregatedVestingSchedules";
 import VestingSchedulerAllowancesTable from "./VestingSchedulesAllowancesTable/VestingSchedulerAllowancesTable";
 import VestingScheduleTable from "./VestingScheduleTable";
-import Link from "../common/Link";
-import { platformApi } from "../redux/platformApi/platformApi";
+
+interface ExecutionWhitelistInfoProps {
+  whitelisted: boolean;
+  network: Network;
+}
+
+const ExecutionWhitelistInfo: FC<ExecutionWhitelistInfoProps> = ({
+  whitelisted,
+  network,
+}) => (
+  <Stack
+    direction="row"
+    alignItems="center"
+    justifyContent="space-between"
+    spacing={1}
+  >
+    <Typography variant="body1" color="secondary">
+      Transaction executed by: {whitelisted ? "Superfluid" : "User"}
+    </Typography>
+
+    {network.vestingContractAddress && (
+      <Stack direction="row" alignItems="center" gap={0.5}>
+        <Typography variant="body1" color="secondary">
+          Contract address
+        </Typography>
+        <Stack direction="row" alignItems="center">
+          <CopyIconBtn
+            TooltipProps={{ placement: "top" }}
+            copyText={getAddress(network.vestingContractAddress)}
+            description="Copy address to clipboard"
+            IconButtonProps={{ size: "small" }}
+          />
+          <Tooltip arrow title="View on blockchain explorer" placement="top">
+            <Link
+              passHref
+              href={network.getLinkForAddress(network.vestingContractAddress)}
+              target="_blank"
+            >
+              <IconButton href="" target="_blank" size="small">
+                <LaunchRoundedIcon color="inherit" />
+              </IconButton>
+            </Link>
+          </Tooltip>
+        </Stack>
+      </Stack>
+    )}
+  </Stack>
+);
 
 interface VestingScheduleTablesProps {}
 
@@ -72,21 +121,22 @@ const VestingScheduleTables: FC<VestingScheduleTablesProps> = ({}) => {
     }
   );
 
-  const { isPlatformWhitelisted } = platformApi.useIsAccountWhitelistedQuery(
-    visibleAddress && network?.platformUrl
-      ? {
-          chainId: network.id,
-          baseUrl: network.platformUrl,
-          account: visibleAddress?.toLowerCase(),
-        }
-      : skipToken,
-    {
-      selectFromResult: (queryResult) => ({
-        ...queryResult,
-        isPlatformWhitelisted: !!queryResult.data,
-      }),
-    }
-  );
+  const { isPlatformWhitelisted, isLoading: isWhitelistLoading } =
+    platformApi.useIsAccountWhitelistedQuery(
+      visibleAddress && network?.platformUrl
+        ? {
+            chainId: network.id,
+            baseUrl: network.platformUrl,
+            account: visibleAddress?.toLowerCase(),
+          }
+        : skipToken,
+      {
+        selectFromResult: (queryResult) => ({
+          ...queryResult,
+          isPlatformWhitelisted: !!queryResult.data,
+        }),
+      }
+    );
 
   const pendingVestingSchedules =
     useAddressPendingVestingSchedules(visibleAddress);
@@ -110,7 +160,7 @@ const VestingScheduleTables: FC<VestingScheduleTablesProps> = ({}) => {
 
   return (
     <Stack
-      gap={2}
+      gap={3.5}
       direction={
         receivedVestingSchedules.length === 0 &&
         mappedSentVestingSchedules.length > 0
@@ -118,7 +168,7 @@ const VestingScheduleTables: FC<VestingScheduleTablesProps> = ({}) => {
           : "column"
       }
     >
-      <Stack gap={2} direction="column">
+      <Stack gap={2}>
         <Typography variant="h6">
           {vestingSchedulesLoading ? (
             <Skeleton width="200px" />
@@ -126,7 +176,7 @@ const VestingScheduleTables: FC<VestingScheduleTablesProps> = ({}) => {
             "Received Vesting Schedules"
           )}
         </Typography>
-        <Card sx={{ p: 0, mb: 3 }}>
+        <Card sx={{ p: 0 }}>
           {vestingSchedulesLoading || receivedVestingSchedules.length > 0 ? (
             <VestingScheduleTable
               dataCy={"received-table"}
@@ -144,85 +194,53 @@ const VestingScheduleTables: FC<VestingScheduleTablesProps> = ({}) => {
         </Card>
       </Stack>
 
-      <Stack gap={2} direction="column">
-        <Typography variant="h6">
+      <Stack gap={3.5}>
+        <Stack gap={2}>
+          <Typography variant="h6">
+            {vestingSchedulesLoading ? (
+              <Skeleton width="200px" />
+            ) : (
+              "Sent Vesting Schedules"
+            )}
+          </Typography>
+          {vestingSchedulesLoading || mappedSentVestingSchedules.length > 0 ? (
+            <Stack gap={3.5}>
+              <AggregatedVestingSchedules
+                vestingSchedules={mappedSentVestingSchedules}
+                network={network}
+              />
+              <VestingScheduleTable
+                data-cy={"created-table"}
+                isLoading={vestingSchedulesLoading}
+                network={network}
+                vestingSchedules={mappedSentVestingSchedules}
+              />
+            </Stack>
+          ) : (
+            <Paper>
+              <NoContentPaper
+                dataCy={"no-created-schedules"}
+                title="No Sent Vesting Schedules"
+                description="Vesting schedules that you have created will appear here."
+              />
+            </Paper>
+          )}
+        </Stack>
+
+        <Stack gap={2}>
           {vestingSchedulesLoading ? (
             <Skeleton width="200px" />
           ) : (
-            "Sent Vesting Schedules"
+            <Typography variant="h6">Allowances and Permissions</Typography>
           )}
-        </Typography>
-        {vestingSchedulesLoading || mappedSentVestingSchedules.length > 0 ? (
-          <>
-            <AggregatedVestingSchedules
-              vestingSchedules={mappedSentVestingSchedules}
+
+          <VestingSchedulerAllowancesTable />
+
+          {!isWhitelistLoading && (
+            <ExecutionWhitelistInfo
               network={network}
+              whitelisted={isPlatformWhitelisted}
             />
-            <VestingScheduleTable
-              data-cy={"created-table"}
-              isLoading={vestingSchedulesLoading}
-              network={network}
-              vestingSchedules={mappedSentVestingSchedules}
-            />
-          </>
-        ) : (
-          <Paper>
-            <NoContentPaper
-              dataCy={"no-created-schedules"}
-              title="No Sent Vesting Schedules"
-              description="Vesting schedules that you have created will appear here."
-            />
-          </Paper>
-        )}
-
-        {vestingSchedulesLoading ? (
-          <Skeleton width="200px" />
-        ) : (
-          <Typography variant="h6">Allowances and Permissions</Typography>
-        )}
-
-        <VestingSchedulerAllowancesTable />
-
-        <Stack
-          direction="row"
-          alignItems="center"
-          justifyContent="space-between"
-          spacing={1}
-        >
-          <Typography variant="body1" color="secondary">
-            Transaction executed by:{" "}
-            {isPlatformWhitelisted ? "Superfluid" : "User"}
-          </Typography>
-
-          {network.vestingContractAddress && (
-            <Stack direction="row" alignItems="center">
-              <Typography variant="body1" color="secondary">
-                Contract address
-              </Typography>
-
-              <CopyIconBtn
-                TooltipProps={{ placement: "top" }}
-                copyText={getAddress(network.vestingContractAddress)}
-                description="Copy address to clipboard"
-              />
-              <Tooltip
-                title="View on blockchain explorer"
-                arrow
-                placement="top"
-              >
-                <Link
-                  passHref
-                  href={network.getLinkForAddress(
-                    network.vestingContractAddress
-                  )}
-                  target="_blank"
-                >
-                  <IconButton href="" target="_blank">
-                    <LaunchRoundedIcon color="inherit" />
-                  </IconButton>
-                </Link>
-              </Tooltip>
-            </Stack>
           )}
         </Stack>
       </Stack>
