9abaf31c981efe92de932d9e259a814f23127396
tokdaniel <tokdaniel4@gmail.com>
2023-03-07 15:15:18 +0100

    fix divider in notifications popover

--- src/components/NotificationsBell/NotificationHeader.tsx
+++ src/components/NotificationsBell/NotificationHeader.tsx
@@ -1,5 +1,5 @@
 import SettingsIcon from "@mui/icons-material/SettingsOutlined";
-import { Stack, Tooltip, Typography } from "@mui/material";
+import { Divider, Stack, Tooltip, Typography } from "@mui/material";
 import Link from "next/link";
 import { FC } from "react";
 import { NotificationTab } from "./NotificationsBell";
@@ -14,46 +14,43 @@ const NotificationHeader: FC<NotificationHeaderProps> = ({
   setActiveTab,
 }) => {
   return (
-    <Stack
-      p={2}
-      sx={{
-        borderBottom: "1px solid #E0E0E0",
-      }}
-      gap={1}
-    >
-      <Stack direction="row" justifyContent="space-between">
-        <Typography variant="h5">Notifications</Typography>
-        <Link href="/settings">
-          <Tooltip title="Open Settings">
-            <SettingsIcon sx={{ cursor: "pointer", color: "GrayText" }} />
-          </Tooltip>
-        </Link>
+    <>
+      <Stack p={2} gap={1}>
+        <Stack direction="row" justifyContent="space-between">
+          <Typography variant="h5">Notifications</Typography>
+          <Link href="/settings">
+            <Tooltip title="Open Settings">
+              <SettingsIcon sx={{ cursor: "pointer", color: "GrayText" }} />
+            </Tooltip>
+          </Link>
+        </Stack>
+        <Stack direction="row" gap={1}>
+          <Typography
+            variant="body2"
+            sx={{
+              textDecoration: activeTab === "new" ? "underline" : "none",
+              cursor: "pointer",
+              color: activeTab === "new" ? "ButtonText" : "GrayText",
+            }}
+            onClick={() => setActiveTab("new")}
+          >
+            New
+          </Typography>
+          <Typography
+            variant="body2"
+            sx={{
+              textDecoration: activeTab === "archive" ? "underline" : "none",
+              cursor: "pointer",
+              color: activeTab === "archive" ? "ButtonText" : "GrayText",
+            }}
+            onClick={() => setActiveTab("archive")}
+          >
+            Archive
+          </Typography>
+        </Stack>
       </Stack>
-      <Stack direction="row" gap={1}>
-        <Typography
-          variant="body2"
-          sx={{
-            textDecoration: activeTab === "new" ? "underline" : "none",
-            cursor: "pointer",
-            color: activeTab === "new" ? "ButtonText" : "GrayText",
-          }}
-          onClick={() => setActiveTab("new")}
-        >
-          New
-        </Typography>
-        <Typography
-          variant="body2"
-          sx={{
-            textDecoration: activeTab === "archive" ? "underline" : "none",
-            cursor: "pointer",
-            color: activeTab === "archive" ? "ButtonText" : "GrayText",
-          }}
-          onClick={() => setActiveTab("archive")}
-        >
-          Archive
-        </Typography>
-      </Stack>
-    </Stack>
+      <Divider />
+    </>
   );
 };
 
