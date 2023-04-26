27d038484c92efd4fe347bac46333843ea4edb91
Mehul Soni <mehul.soni.estonia@gmail.com>
2023-04-26 16:11:52 +0300

    fixed twitter og image and removed SEO tag

--- src/pages/superfluid-runner.tsx
+++ src/pages/superfluid-runner.tsx
@@ -1,7 +1,6 @@
 import withStaticSEO from "../components/SEO/withStaticSEO";
 import {FC, PropsWithChildren} from "react";
-import SEO from "../components/SEO/SEO";
-import {Alert, AlertTitle, Container, Typography} from "@mui/material";
+import {Alert, Container, Typography} from "@mui/material";
 import {NextPage} from "next";
 import Minigame from "../features/minigame/MinigameContainer";
 import config from "../utils/config";
@@ -18,26 +17,18 @@ const DisplayWarning: FC<PropsWithChildren<{}>> = ({}) => (
     </Container>
 )
 
-const SuperfluidRunnerPageContainer: FC<PropsWithChildren<{}>
-> = ({children}) => (
-    <SEO
-        title={`Superfluid Runner | Superfluid`}
-        ogTitle={`Superfluid Runner | Superfluid`}
-        ogImage={`${config.appUrl}/images/superfluid-runner-thumbnail.png`}
-    >
-        {children}
-    </SEO>
-);
-
 const SuperfluidRunner: NextPage = () => {
 
     const {isConnected} = useAccount();
 
     return (
-        <SuperfluidRunnerPageContainer key={`superfluid-runner`}>
+        <>
             <Minigame/>
             {!isConnected && <DisplayWarning/>}
-        </SuperfluidRunnerPageContainer>
+        </>
     );
 }
-export default withStaticSEO({title: "Superfluid Runner | Superfluid"}, SuperfluidRunner);
+export default withStaticSEO({
+    title: "Superfluid Runner | Superfluid",
+    ogImage: `${config.appUrl}/images/superfluid-runner-thumbnail.png`
+}, SuperfluidRunner);
