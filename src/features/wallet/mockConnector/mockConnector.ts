54a4b5298e46ea49939db731a9d9c400f8b54ce1
Kaspar Kallas <kaspar@superfluid.finance>
2023-06-19 15:16:42 +0300

    try to get mockSigner to work with Eip1193Bridge

--- src/features/wallet/mockConnector/mockConnector.ts
+++ src/features/wallet/mockConnector/mockConnector.ts
@@ -1,6 +1,8 @@
 import { Chain, Wallet } from "@rainbow-me/rainbowkit";
 import { MockConnector } from "wagmi/connectors/mock";
 import { createWalletClient, custom } from "viem";
+import { Signer } from "ethers";
+import { Eip1193Bridge } from "@ethersproject/experimental";
 
 export interface MockConnectorOptions {
   chains: Chain[];
@@ -13,13 +15,14 @@ const mockConnector = ({ chains }: MockConnectorOptions): Wallet => ({
   iconUrl: "/icons/icon-96x96.png",
   iconBackground: "#000000",
   createConnector: () => {
-    const mockSigner = (window as any).mockSigner;
+    const mockSigner = (window as any).mockSigner as Signer;
+    const eip1193Bridge = new Eip1193Bridge(mockSigner, mockSigner.provider);
     return {
       connector: new MockConnector({
         chains,
         options: {
           walletClient: createWalletClient({
-            transport: custom(mockSigner.provider.provider as any),
+            transport: custom(eip1193Bridge),
           }),
         },
       }),
