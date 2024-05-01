4b5a0fb0278cf622ee3e0b7ddbdf3433ebe6cae8
Kaspar Kallas <kaspar@superfluid.finance>
2024-05-01 16:57:00 +0300

    fix whitelisting for testnets without deployed platform

--- src/features/network/networks.ts
+++ src/features/network/networks.ts
@@ -817,6 +817,7 @@ export const networkDefinition = {
       sfMeta.getNetworkByChainId(chainIds.optimismSepolia),
       chainIds.optimismSepolia
     ),
+    testnet: true,
     blockExplorers: ensureDefined(chain.optimismSepolia.blockExplorers),
     slugName: "opsepolia",
     v1ShortName: "opsepolia",
@@ -940,8 +941,9 @@ export const getNetworkDefaultTokenPair = memoize(
 
 // The vesting contract might be deployed to more networks but we check for the existence of the Platform.;
 export const vestingSupportedNetworks = allNetworks
-  .filter((network) => network.platformUrl)
-  .concat([networkDefinition.base]) // TODO: Remove when The Platform is deployed to Base.
+  .filter(
+    (network) => network.vestingContractAddress
+  )
   .sort((n1, n2) => (!n1.testnet && n2.testnet ? -1 : 1));
 
 export const deprecatedNetworkChainIds = [
