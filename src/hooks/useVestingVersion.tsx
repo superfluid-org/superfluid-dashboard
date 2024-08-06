5b032d6464d8ab4c46b39e7746bd87414d4100ac
Kaspar Kallas <git@kasparkallas.com>
2024-08-06 21:19:06 +0300

    allow setting vesting version when wallet not connected

--- src/hooks/useVestingVersion.tsx
+++ src/hooks/useVestingVersion.tsx
@@ -1,4 +1,4 @@
-import { Address, useAccount } from "wagmi";
+import { useAccount } from "wagmi";
 import { useHasFlag } from "../features/flags/flagsHooks";
 import { useExpectedNetwork } from "../features/network/ExpectedNetworkContext";
 import { Flag, setVestingSchedulerFlag } from "../features/flags/flags.slice";
@@ -12,15 +12,15 @@ export const useVestingVersion = (props?: { network?: Network }) => {
     const network = props?.network ?? expectedNetwork;
 
     const { address: accountAddress } = useAccount();
-    const hasVestingV2Enabled = useHasFlag(
-        accountAddress
-            ? {
-                type: Flag.VestingScheduler,
-                chainId: network.id,
-                account: getAddress(accountAddress),
-                version: "v2"
-            }
-            : undefined
+
+    const flagAddress = accountAddress ?? "0x0000000000000000000000000000000000000000" // Use zero address as key when wallet not connected.
+
+    const hasVestingV2Enabled = useHasFlag({
+        type: Flag.VestingScheduler,
+        chainId: network.id,
+        account: getAddress(flagAddress),
+        version: "v2"
+    }
     );
 
     const dispatch = useDispatch();
@@ -30,14 +30,12 @@ export const useVestingVersion = (props?: { network?: Network }) => {
             version: "v1" | "v2"
         }
     ) => {
-        if (accountAddress) {
-            dispatch(setVestingSchedulerFlag({
-                account: getAddress(accountAddress),
-                chainId: input.chainId,
-                version: input.version
-            }));
-        }
-    }, [dispatch, accountAddress]);
+        dispatch(setVestingSchedulerFlag({
+            account: getAddress(flagAddress),
+            chainId: input.chainId,
+            version: input.version
+        }));
+    }, [dispatch, flagAddress]);
 
     return useMemo(() => ({
         vestingVersion: hasVestingV2Enabled ? "v2" : "v1",
