6524229ca6aeb42cc6acebd207cc50d07fc92b69
elvijsTDL <elvijs.dzirkals@testdevlab.com>
2025-05-15 21:58:58 +0300

    Try to add mock connector like for safeApps and stop re-trying

--- src/features/wallet/WagmiManager.tsx
+++ src/features/wallet/WagmiManager.tsx
@@ -1,6 +1,6 @@
 import { FC, PropsWithChildren, useEffect, useState } from "react";
 import { WagmiProvider } from "wagmi";
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { wagmiConfig, resolvedWagmiClients } from "./wagmiConfig";
 import { WalletWeirdnessHandler } from "../../components/WalletWeirdnessHandler/WalletWeirdnessHandler";
 
@@ -9,18 +9,10 @@ export { wagmiConfig, resolvedWagmiClients };
 export const tanstackQueryClient = new QueryClient();
 
 const WagmiManager: FC<PropsWithChildren> = ({ children }) => {
-  const [reconnectOnMount, setReconnectOnMount] = useState(false);
-
-  useEffect(() => {
-    // It's a bit of a funny fix to avoid hydration errors. The server-side pages are statically generated without a wallet connected.
-    setReconnectOnMount(true)
-  }, [])
+  const reconnectOnMount = true;
 
   return (
-    <WagmiProvider
-      config={wagmiConfig}
-      reconnectOnMount={reconnectOnMount}
-    >
+    <WagmiProvider config={wagmiConfig} reconnectOnMount={reconnectOnMount}>
       <QueryClientProvider client={tanstackQueryClient}>
         {children}
         <WalletWeirdnessHandler />
