cb6a2faf2048785c1adb8ee334a9211fa21684d9
Kaspar Kallas <kaspar@superfluid.finance>
2026-03-20 17:18:36 +0200

    Fix type error

--- src/geofencing.ts
+++ src/geofencing.ts
@@ -1,28 +1,28 @@
-export const blockedCountries = [
-    'AF', // Afghanistan
-    'BY', // Belarus
-    'BI', // Burundi
-    'CF', // Central African Republic
-    'CU', // Cuba
-    'CD', // Democratic Republic of the Congo
-    'ET', // Ethiopia
-    'IR', // Iran
-    'IQ', // Iraq
-    'LB', // Lebanon
-    'LY', // Libya
-    'ML', // Mali
-    'MM', // Myanmar (Burma)
-    'NI', // Nicaragua
-    'KP', // North Korea
-    'RU', // Russia
-    'SO', // Somalia
-    'SS', // South Sudan
-    'SD', // Sudan
-    'SY', // Syria
-    'UA', // Ukraine
-    'VE', // Venezuela
-    'YE', // Yemen
-    'ZW', // Zimbabwe
-  ]
-  
-  export const blockedRegions = []  
+ export const blockedCountries = [
+     "AF", // Afghanistan
+     "BY", // Belarus
+     "BI", // Burundi
+     "CF", // Central African Republic
+     "CU", // Cuba
+     "CD", // Democratic Republic of the Congo
+     "ET", // Ethiopia
+     "IR", // Iran
+     "IQ", // Iraq
+     "LB", // Lebanon
+     "LY", // Libya
+     "ML", // Mali
+     "MM", // Myanmar (Burma)
+     "NI", // Nicaragua
+     "KP", // North Korea
+     "RU", // Russia
+     "SO", // Somalia
+     "SS", // South Sudan
+     "SD", // Sudan
+     "SY", // Syria
+     "UA", // Ukraine
+     "VE", // Venezuela
+     "YE", // Yemen
+     "ZW", // Zimbabwe
+ ]
+
+ export const blockedRegions: string[] = []
